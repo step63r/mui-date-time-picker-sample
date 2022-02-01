38630bd88fa2ec4dadf355cac98ef850c7a67bca
Ryosuke Uchiyama <uchi63rs@yahoo.co.jp>
2022-02-02 00:00:14 +0900

    initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+import {
+  Stack,
+  TextField
+} from '@mui/material';
+
+import {
+  DesktopDatePicker,
+  MobileDatePicker,
+  TimePicker,
+  LocalizationProvider,
+} from '@mui/lab';
+
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+
+const App = () => {
+  const [value, setValue] = useState<Date | null>(new Date());
+
+  const handleChange = (newValue: Date | null) => {
+    setValue(newValue);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Stack spacing={3} margin={4}>
+        <DesktopDatePicker
+          label="Date desktop"
+          inputFormat="yyyy/MM/dd"
+          value={value}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+        <MobileDatePicker
+          label="Date mobile"
+          inputFormat="yyyy/MM/dd"
+          value={value}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+        <TimePicker
+          ampm={false}
+          label="Time"
+          value={value}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </Stack>
+    </LocalizationProvider>
   );
 }
 
